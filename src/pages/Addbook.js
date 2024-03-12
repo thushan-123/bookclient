e6857c19d30d0ceab4ed190b7fbc1fb0bb1d8f38
thushan-123 <162820036+thushan-123@users.noreply.github.com>
2024-03-12 22:04:20 +0530

    Update Addbook.js
    ok

--- src/pages/Addbook.js
+++ src/pages/Addbook.js
@@ -39,7 +39,7 @@ export default function Addbook() {
         console.log(err)
       })
       */
-      await axios.post("http://localhost:8000/Book/Uploading/Data/",dataset).then((res)=>{
+      await axios.post("http://104.43.111.192:8000//Book/Uploading/Data/",dataset).then((res)=>{
         console.log(res)
         if(res.data.status==="inserted"){
           window.alert("Insert Complete")
