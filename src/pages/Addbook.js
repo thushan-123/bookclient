0fe404f3c9116fce91abd5ab0debf3d0ab9806f9
thushan-123 <162820036+thushan-123@users.noreply.github.com>
2024-03-12 17:34:26 +0530

    Update Addbook.js
    ok

--- src/pages/Addbook.js
+++ src/pages/Addbook.js
@@ -39,7 +39,7 @@ export default function Addbook() {
         console.log(err)
       })
       */
-      await axios.post("https://bk-4.onrender.com/Book/Uploading/Data/",dataset).then((res)=>{
+      await axios.post("http://localhost/Book/Uploading/Data/",dataset).then((res)=>{
         console.log(res)
         if(res.data.status==="inserted"){
           window.alert("Insert Complete")
